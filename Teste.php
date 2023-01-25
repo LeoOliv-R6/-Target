1b4a57bf27c191dbde3618068bf9303cc290d8a9
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-01-25 12:10:17 -0300

    first commit

---
+++ Teste.php
@@ -0,0 +1,20 @@
+
+<?php
+$INDICE = 13;
+$SOMA = 0;
+$K = 0;
+
+while ($K < $INDICE)
+{
+ $K++;
+//  echo "</br>";
+//  echo $K;
+//  echo "</br>";
+ $SOMA = $SOMA + $K;
+//  echo "</br>";
+//  echo $SOMA;
+//  echo "</br>";
+}
+echo "O resultado da soma é ". $SOMA;
+
+?>
