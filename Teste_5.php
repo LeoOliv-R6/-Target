d42c3f8cca8c3784038f5f771e833e2aa1672f48
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-01-25 15:25:45 -0300

    função para frase invertida

--- Teste_5.php
+++ Teste_5.php
@@ -1,3 +1,10 @@
 <?php
+$str = "Essa frase deve ficar invertida";
+$revstr = "";
+$contador = strlen($str) - 1;
 
+for ($i = $contador; $i >= 0; $i--) {
+    $revstr .= $str[$i];
+}
+echo $revstr;
 ?>
