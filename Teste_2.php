137eddba9374dc038596daedacfbe76db4234eab
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-01-25 13:08:30 -0300

    Segundo teste

---
+++ Teste_2.php
@@ -0,0 +1,21 @@
+<html>
+<?php
+$inicio = 0;
+$fim = 1;
+
+echo "Sequência de 1 a 100: <br />";
+echo $fim;
+
+while($fim <= 100) {
+    $fim = $inicio + $fim;
+    $inicio = $fim - $inicio;
+
+    if ($fim <=100) {
+        echo ",". $fim;
+    } else {
+        echo ".";
+    }
+}
+?>
+
+</html>
