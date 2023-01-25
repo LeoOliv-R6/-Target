fda22af1f3717c0701319f72c16771865e9d21d8
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-01-25 14:58:24 -0300

    melhorado

--- Teste_4.php
+++ Teste_4.php
@@ -5,12 +5,15 @@ $MG = 29229.88;//29.229,88 = 16.170548370275%
 $ES = 27165.48;//27.165,48 = 15.028481414968%
 $Outros = 19849.53;//19.849,53 = 10.981153018494%
 
-$estados = [$SP, $RJ, $MG, $ES, $Outros];
-$total = array_sum($estados);
-
-foreach($estados as $estado){
-    $porcentagem = ($estado*100)/$total;
-    echo "A porcentagem de contribuição é de ".round($porcentagem, 2). "<br>";
+$i=0;
+$contri = [$SP, $RJ, $MG, $ES, $Outros];
+$estados = array("SP","RJ","MG","ES","Outros");
+$total = array_sum($contri);
+foreach ($contri as $mensal) {
+    $pct = ($mensal*100)/$total;
+    $estado = $estados[$i];
+    echo "O estado $estado contribui com ". round($pct)." %<br>";
+    $i++;
 }
 // echo $total; //180.759,98 = 100%
 ?>
