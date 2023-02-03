370cfa41175356c543d95a51fa4a8cc7e411881e
LeoOliv-R6 <leonardo.oliv182@gmail.com>
2023-02-03 11:44:19 -0300

    criando um site sobre skins do fortnite

--- Teste_2.php
+++ Teste_2.php
@@ -1,17 +1,18 @@
 <html>
 <?php
+
 $inicio = 0;
 $fim = 1;
 
 echo "Sequência de 1 a 100: <br />";
 echo $fim;
 
-while($fim <= 100) {
+while ($fim <= 100) {
     $fim = $inicio + $fim;
     $inicio = $fim - $inicio;
 
-    if ($fim <=100) {
-        echo ",". $fim;
+    if ($fim <= 100) {
+        echo "," . $fim;
     } else {
         echo ".";
     }
---
+++ Teste_6.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>SOURCHNITE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <H1>SOURCHNITE</H1>
+
+        <fieldset class="fort">
+            <legend>Escolha uma skin:</legend>
+
+            <div>
+                <input type="checkbox" id="Banana" name="Banana" checked>
+                <label for="Banana">Banana</label>
+            </div>
+
+            <div>
+                <input type="checkbox" id="Jonesys" name="Jonesys">
+                <label for="Jonesys">Jonesys</label>
+            </div>
+            <div>
+                <input type="checkbox" id="Aboborado" name="Aboborado">
+                <label for="Aboborado">Aboborado Cromado</label>
+            </div>
+            <div>
+                <input type="checkbox" id="Piccolo" name="Piccolo">
+                <label for="Piccolo">Piccolo</label>
+            </div>
+            <div>
+                <input type="checkbox" id="Gohan" name="Gohan">
+                <label for="Gohan">Gohan</label>
+            </div>
+            <div>
+                <input type="checkbox" id="Goku" name="Goku">
+                <label for="Goku">Goku</label>
+            </div>
+        </fieldset>
+    </div>
+    <?php
+
+    ?>
+</body>
+
+</html>
